dd4a04c91afceb039081f6439c2aa1b0b3c08b17
Nathan Beck <njosefbeck@gmail.com>
2016-12-08 20:37:22 -0600

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+// This accordion is for the New Clients page
+
+$(function() {
+	$( "#accordion" ).accordion({
+		collapsible: true,
+		active: false,
+		heightStyle: "content"
+	});
+});
+
+// Map API for Contacts page
+
+function initMap() {
+  
+  var myLatLng = {lat: 38.605396, lng: -90.345761};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    zoom: 15,
+    disableDefaultUI: true,
+    draggable: false,
+    zoomControl: false
+  });
+
+  var marker = new google.maps.Marker({
+  	position: myLatLng,
+  	map: map,
+  	title: 'St. Louis Wellness Center'
+  });
+
+}
